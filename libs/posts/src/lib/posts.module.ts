c9595515c90944f5981ef552fe9945485ad959b6
Eduardo Vargas <eduardo704@gmail.com>
2021-04-03 17:07:06 -0300

    cleaning up

--- libs/posts/src/lib/posts.module.ts
+++ libs/posts/src/lib/posts.module.ts
@@ -3,18 +3,10 @@ import { PostsRoutingModule } from './posts.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostListComponent } from './post-list/post-list.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
 
 @NgModule({
   imports: [CommonModule, PostsRoutingModule],
-  declarations: [
-    PostListComponent,
-    PostCardComponent,
-    PostDetailComponent
-
-  ],
-  exports: [
-    PostListComponent
-  ],
+  declarations: [PostListComponent, PostCardComponent],
+  exports: [PostListComponent],
 })
 export class PostsModule {}
